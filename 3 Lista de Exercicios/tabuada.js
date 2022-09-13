9212a691af868782186cc76ac17e59de381af4b5
GhostBlkz <111324297+GhostBlkz@users.noreply.github.com>
2022-09-13 09:02:56 -0300

    Add files via upload

--- 3 Lista de Exercicios/tabuada.js
+++ 3 Lista de Exercicios/tabuada.js
@@ -1,25 +1,25 @@
-function calcular(){
+function calcular() {
     console.log('Funcionou')
- 
+
     //com estrutura de repetição while
-    
+
     let num = 1
 
-    while (num <=10){
+    while (num <= 10) {
         let contador = 0
-        while (contador <=10){
-            console.log(`${num} x ${contador} = ${num*contador}`)
+        while (contador <= 10) {
+            console.log(`${num} x ${contador} = ${num * contador}`)
             contador++
         }
         num++
     }
 }
-function calculaMedia(){
+function calculaMedia() {
     let altura
     let media
     let contador = 0
     let acumula = 0
-    while(contador <=10){
+    while (contador <= 10) {
         altura = prompt('Informe a altura')
         acumula = acumula + altura
         contador++
@@ -27,7 +27,7 @@ function calculaMedia(){
 
     media = acumula / 10
 
-    document.getElementById("resul")= `A media das alturas é ${media}`
+    document.getElementById("resul") = `A media das alturas é ${media}`
 
 
 
