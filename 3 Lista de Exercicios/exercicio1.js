7dc9f74eebc1ebd7101b575fe2c070b691657fda
GhostBlkz <111324297+GhostBlkz@users.noreply.github.com>
2022-09-13 08:40:28 -0300

    Add files via upload

---
+++ 3 Lista de Exercicios/exercicio1.js
@@ -0,0 +1,34 @@
+function calcularExe1(){
+    for(let j = 1; j < 6 ; j++){
+    let a = Number(prompt("Informe o Primeiro numero"))
+    let b = Number(prompt("Informe o Segundo numero"))
+    let c = Number(prompt("Informe o Terceiro numero"))
+    let d = Number(prompt("Informe o Quarto numero"))
+    let aux = 0
+
+    console.log(`Ordem lida ${a} - ${b} - ${c} - ${d} `)
+
+    for (let i =1 ; i <=3; i++){
+        if (a > b){
+            aux = a
+            a = b
+            b = aux
+        }
+        if ( b > c){
+            aux = b
+            b = c
+            c = aux
+        }
+        if (c > d){
+            aux = c
+            c = d
+            d = aux      
+        }
+    }
+    //mostra o resultado
+    console.log(`Crescente ${a} - ${b} - ${c} - ${d}`)
+    console.log(`Decrescente ${d} - ${c} - ${b} - ${a}`)
+    console.log(``)
+    }   
+
+}
